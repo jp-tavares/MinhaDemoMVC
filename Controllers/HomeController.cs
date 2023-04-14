f781ca8a4688a98b553e08dbe112c02dd39daf83
Tavares <jptavares.dev@gmail.com>
2023-04-14 14:15:16 -0300

    adiciona exemplos de retorno

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,8 +4,6 @@ using System.Diagnostics;
 
 namespace MinhaDemoMVC.Controllers
 {
-    [Route("")]
-    [Route("gestao-clientes")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -15,23 +13,24 @@ namespace MinhaDemoMVC.Controllers
             _logger = logger;
         }
 
-        [Route("")]
-        [Route("pagina-inicial")]
-        [Route("pagina-inicial/{id}/{categoria:guid}")]
         public IActionResult Index(string id, Guid categoria)
         {
             return View();
         }
 
-        [Route("privacidade")]
-        [Route("politica-de-privacidade")]
         public IActionResult Privacy()
         {
-            return View();
+            //return Json("{'nome': 'Eduardo'}");
+
+            //var fileBytes = System.IO.File.ReadAllBytes(@"c:\arquivo.txt");
+            //var fileName = "arquivo.txt";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            //return Content("Qualquer coisa");
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        [Route("erro-encontrado")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
